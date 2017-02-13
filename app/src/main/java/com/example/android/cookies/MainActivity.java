a1f836ba3fb9ac2a2fc5a196afe4e29fad965afe
Thom Kok <tkok@roc-dev.com>
2017-02-13 17:58:56 +0100

    update

--- app/src/main/java/com/example/android/cookies/MainActivity.java
+++ app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.android.cookies;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,9 +21,19 @@ public class MainActivity extends AppCompatActivity {
      * Called when the cookie should be eaten.
      */
     public void eatCookie(View view) {
-        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        ImageView cookieView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        cookieView.setImageResource(R.drawable.after_cookie);
+        TextView statusView = (TextView) findViewById(R.id.status_text_view);
+        statusView.setText("I'm so full");
+    }
 
-        // TODO: Find a reference to the TextView in the layout. Change the text.
-
+    /**
+     * Called when the cookie should be given.
+     */
+    public void giveCookie(View view) {
+        ImageView cookieView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        cookieView.setImageResource(R.drawable.before_cookie);
+        TextView statusView = (TextView) findViewById(R.id.status_text_view);
+        statusView.setText("I'm so hungry");
     }
 }

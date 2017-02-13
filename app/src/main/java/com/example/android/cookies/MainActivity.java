2d5724623415bba431994021dd18e5dc0899852e
Thom Kok <tkok@roc-dev.com>
2017-02-13 17:22:19 +0100

    first commit

---
+++ app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.cookies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+
+    }
+}

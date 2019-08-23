f0f60f041bc2ef68d4126f7f6098626c81771e8d
Thomkok <thomkok13@hotmail.com>
2019-08-23 23:08:19 +0200

    Update

--- app/src/main/java/com/example/android/cookies/MainActivity.java
+++ app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.android.cookies;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 
 public class MainActivity extends AppCompatActivity {
